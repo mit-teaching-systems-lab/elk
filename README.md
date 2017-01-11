f9661cc452f89aade1e04df711ea51fab1351de5
Tiffany Wong <tcwong@mit.edu>
2017-01-11 15:05:22 -0500

    React starter

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # elk
 An educational, roleplay game for teachers to practice eliciting learner knowledge
+
+Setup
+---
+ 
+```
+npm install
+```
+ 
+ 
+ 
+Compile
+---
+ 
+```
+npm run compile
+```
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
+
 console.log("Hello World!");
