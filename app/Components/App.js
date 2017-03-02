8c910c3a605aa6aa9896370a4bfc26b41afec9a0
Tiffany Wong <tcwong@mit.edu>
2017-03-01 21:28:00 -0500

    added buttons

--- app/Components/App.js
+++ app/Components/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <button> Create New Game </button>
+        <button> Join Game </button>
       </div>
     );
   }
