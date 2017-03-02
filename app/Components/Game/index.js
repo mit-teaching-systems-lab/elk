6050b4681bfef870048f455094e822974d50157b
Tiffany Wong <tcwong@mit.edu>
2017-03-02 18:55:34 -0500

    if gameID doesn't exist, user cannot access game via url

--- app/Components/Game/index.js
+++ app/Components/Game/index.js
@@ -9,16 +9,18 @@ let socket = io.connect('');
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {at_capacity: false, role: null, round_over: false};
+    this.state = {at_capacity: false, role: null, round_over: false, is_active_game: false};
     socket.on('connect', () => this._connect());
     socket.on('init', (username) =>this._initialize(username));
     socket.on('getroom', () => this._get_room());
     socket.on('adduser', (user_role) => this._add_user(user_role));
     socket.on('fullhouse', () => this._full_house());
+    socket.on('isgameID', (flag) => this._is_game_ID(flag));
   }
 
   componentDidMount() {
-    socket.emit('setroom', this.props.params.gameID);
+    // socket.emit('setroom', this.props.params.gameID);
+    socket.emit('joingame', this.props.params.gameID);
   }
 
   _connect() {
@@ -27,6 +29,13 @@ class Game extends React.Component {
 
   _initialize(username) {
     this.setState({user: username});
+  }
+
+  _is_game_ID(flag) {
+    this.setState({is_active_game: flag});
+    if (flag) {
+      this._connect();
+    }
   }
 
   _add_user(user_role) {
@@ -47,6 +56,14 @@ class Game extends React.Component {
   }
 
   render() {
+    var gameID = this.props.params.gameID;
+    console.log("game ID extracted from URL: " + gameID);
+    console.log("gameID type: " + typeof gameID);
+    if (!this.state.is_active_game) {
+      return (
+        <h1> This page does not exit </h1>
+      );
+    }
     if (this.state.at_capacity) {
       return (
           <h1>Full room</h1>
